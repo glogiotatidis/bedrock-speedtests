9013357dd9be0052c5b6b209cfd59db0b766ad64
Giorgos Logiotatidis <seadog@sealabs.net>
2015-12-09 13:26:31 -0500

    Remove old tests.

--- test.sh
+++ test.sh
@@ -5,5 +5,7 @@ URL=${URL:-"https://www.mozilla.com/firefox/new"}
 POLL=${POLL:-5}
 TIMEOUT=${TIMEOUT:-360}
 
+rm results/*.xml
+
 docker pull giorgos/webpagetest
 docker run -v `pwd`:/src giorgos/bedrock-speedtests test $URL --key $KEY --location $LOCATION --poll $POLL --timeout $TIMEOUT --breakdown --pagespeed --reporter xunit --specs specs.json > results/${LOCATION}.xml
