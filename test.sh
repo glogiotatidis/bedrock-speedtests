566fadf8de97e148920bf1d829b0cf89d4a1b682
Giorgos Logiotatidis <seadog@sealabs.net>
2015-12-09 10:29:58 -0500

    Fix default timeout value.

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -xe
 LOCATION=${LOCATION:-"Dulles_IE11"}
 URL=${URL:-"https://www.mozilla.com/firefox/new"}
 POLL=${POLL:-5}
-TIMEOUT=${TIMEOUT:360}
+TIMEOUT=${TIMEOUT:-360}
 
 docker pull giorgos/webpagetest
 docker run -v `pwd`:/src giorgos/bedrock-speedtests test $URL --key $KEY --location $LOCATION --poll $POLL --timeout $TIMEOUT --breakdown --pagespeed --reporter xunit --specs specs.json > results/results.xml
