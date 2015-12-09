17222ddb2fe42d656be5f08f19640b39364c1acd
Giorgos Logiotatidis <seadog@sealabs.net>
2015-12-09 14:44:53 -0500

    New specs.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ URL=${URL:-"https://www.mozilla.com/firefox/new"}
 POLL=${POLL:-5}
 TIMEOUT=${TIMEOUT:-360}
 
-rm results/*.xml
+rm -rf results/*.xml
 
 docker pull giorgos/webpagetest
 docker run -v `pwd`:/src giorgos/bedrock-speedtests test $URL --key $KEY --location $LOCATION --poll $POLL --timeout $TIMEOUT --breakdown --pagespeed --runs 1 --first --reporter xunit --specs specs.json > results/${LOCATION}.xml
